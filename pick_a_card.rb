b6e24de914dfcd100001db66ddaaf79d411b46cc
buchase <b004es@gmail.com>
2022-01-21 12:59:41 +0200

    ubral lishniy + in puts

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -6,4 +6,4 @@ values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 # Буби, черви, крести, пики.
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
 
-puts "#{values.sample} of + #{suits.sample}"
+puts "#{values.sample} of #{suits.sample}"
